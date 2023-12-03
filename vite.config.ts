ff7ca9fd808632f8af04ac528282c54467d32623
Thino <rakotomalalathino@gmail.com>
2023-12-03 22:55:51 +0300

    Updating repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/WEEKLY_DECEMBER_FRONT_1/",
+  appType:"mpa",
   optimizeDeps: {
     include: ['/src/utils/json_utils.js'],
   },
