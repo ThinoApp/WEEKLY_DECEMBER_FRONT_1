975c7604efc86bc8e2fcc6604360b0ffce5c30b2
Thino <rakotomalalathino@gmail.com>
2023-12-03 23:05:05 +0300

    Updating repo

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import './index.scss';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App';
-const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <App />,
-    },
-]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <RouterProvider router={router} />
+        <App />
     </React.StrictMode>,
 );
